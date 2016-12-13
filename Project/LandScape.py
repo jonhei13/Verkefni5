bf4b613eeaa17369ccbcae6c7cf2f9b28f63a7a6
jonhei13 <jonhei13@ru.is>
2016-12-13 22:34:58 +0000

    Terrain

--- Project/LandScape.py
+++ Project/LandScape.py
@@ -1,21 +1,26 @@
 import pygame
 
+SCREEN_HEIGHT = 1280
+SCREEN_WIDTH = 720
 class Landscape(pygame.sprite.Sprite):
-
-    def __init__(self, color, width, height):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-
-        self.moveY = 0
-        self.moveX = 0
-        self.image.fill('0,0,255')
-        self.image = pygame.Surface([width, height])
-        self.player = None
-        self.rect = self.image.get.rect()
+        super().__init__()
+        self.image = pygame.image.load('Pics/SmallTerrain.png').convert()
+        self.rect = self.image.get_rect()
 
 
-    def update(self):
-        #See if we hit the player
-        hit = pygame.sprite.collide_rect(self,self.player)
 
-        #Movement Up And Down
-        self.rect.y += self.moveY
+    # def GetImage(self):
+    #
+    #     image = pygame.Surface([])
+    #     image.blit(self,(0,0), ())
+    #     image.set_colorkey('0,0,255')
+    #     return image
+    #
+    # def update(self):
+    #     #See if we hit the player
+    #     hit = pygame.sprite.collide_rect(self,self.player)
+    #
+    #     #Movement Up And Down
+    #     self.rect.y += self.moveY
--- Project/TheGame.py
+++ Project/TheGame.py
@@ -2,20 +2,22 @@ import pygame
 import sys
 from Project import LandScape
 
-pygame.init()
-screen_x = 1280
-screen_y = 720
-screen = pygame.display.set_mode((screen_x, screen_y))
-pygame.display.set_caption('Worms2d')
-
-done = False
 def main():
-    while not done:
+    pygame.init()
+    screen_x = 1280
+    screen_y = 720
+
+    screen = pygame.display.set_mode((screen_x, screen_y))
+    Terrain = LandScape.Landscape()
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                done = True
+                pygame.quit()
                 sys.exit()
+        screen.blit(Terrain.image, Terrain.rect)
+        pygame.display.update()
 
-        screen.bl
+
 
 main()
