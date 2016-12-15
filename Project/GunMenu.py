877e87ddec267dbff30cb9260367782d3bfe38f4
dadi15 <dadi15@ru.is>
2016-12-15 17:17:18 +0000

    Gun menu class

--- Project/GunMenu.py
+++ Project/GunMenu.py
@@ -4,15 +4,15 @@ import pygame
 class GunMenu:
     def __init__(self):
         self.chosen = 1
-
-    def choose_gun(self, get_image):
-        self.img_list = []
-        self.chosen = get_image
+        self.img_list = list()
 
         self.img_list.append(pygame.transform.scale(pygame.image.load('Pics/GunMenu/FirstChosen.png'), (210, 50)))
         self.img_list.append(pygame.transform.scale(pygame.image.load('Pics/GunMenu/SecondChosen.png'), (210, 50)))
         self.img_list.append(pygame.transform.scale(pygame.image.load('Pics/GunMenu/ThirdChosen.png'), (210, 50)))
         self.img_list.append(pygame.transform.scale(pygame.image.load('Pics/GunMenu/FourthChosen.png'), (210, 50)))
+
+    def choose_gun(self, get_image):
+        self.chosen = get_image
         return self.img_list[get_image]
 
 
