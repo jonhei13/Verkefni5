7c85a6c8ce4783273d0bcb5819c6ede567291be0
dadi15 <dadi15@ru.is>
2016-12-17 21:16:40 +0000

    Team rotation

--- Project/Team.py
+++ Project/Team.py
@@ -1,12 +1,14 @@
 class Team:
+    members = []
 
     def __init__(self, team_list):
-        self.team_members = team_list
+        self.members = team_list
         self.team_health = 0
 
     def update(self, team_list):
         self.team_health = 0
-        for member in team_list:
+        self.members = team_list
+        for member in self.members:
             self.team_health += member.life
 
 
